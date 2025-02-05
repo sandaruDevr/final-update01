ef7c65fc7d3a70c86b92b347eff7ee5e5a43ae1d
sandaruDevr <sadaludev@gmail.com>
2025-02-05 08:02:36 -0800

    Update PricingPage.tsx

--- src/pages/pricing/PricingPage.tsx
+++ src/pages/pricing/PricingPage.tsx
@@ -14,8 +14,10 @@ export function PricingPage() {
       />
       <div className="min-h-screen bg-gray-50">
         <div className="max-w-7xl mx-auto px-4 py-16 sm:px-6 lg:px-8">
-          <PricingHeader />
-          <PricingTiers />
+          <PricingHeader /> 
+          <div className="flex justify-center">
+  <PricingTiers />
+</div>
         </div>
       </div>
     </>
